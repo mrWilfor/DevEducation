fa000bfd2631e1f88cb857bcfe68481e2cf08247
Tarasov Ivan <mr.wilfor@gmail.com>
2020-04-19 07:27:06 +0300

    correct

--- src/main/java/dfhf.java
+++
@@ -1,9 +0,0 @@
-import HomeWork.hw_14_04_2020.ArithmeticOperation;
-import HomeWork.hw_14_04_2020.ArithmeticOperation.*;
-public class dfhf {
-    public static void main(String[] args) {
-
-        System.out.println(ArithmeticOperation.divisionByRemainderCharByte('O', (byte) 6));
-
-    }
-    }
