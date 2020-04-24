c38f0156cabe8e390c8454fac247b60c0d6d422c
Tarasov Ivan <mr.wilfor@gmail.com>
2020-04-24 21:30:50 +0300

    PushPush

--- src/test/java/Practice/Practic23_04_2020/AscendingOrDescendingTest.java
+++ src/test/java/Practice/Practic23_04_2020/AscendingOrDescendingTest.java
@@ -8,7 +8,12 @@ class AscendingOrDescendingTest {
 
     @Test
     void sequenceTest() {
-        String input = "1\n" + "2\n" + "3\n" + "0\n";
-        assertTrue(AscendingOrDescending.sequence(new Scanner(input)), "Test failed");
-    }
+        String input1 = "1\n" + "2\n" + "3\n" + "0\n";
+        String input2 = "3\n" + "2\n" + "1\n" + "0\n";
+        String input3 = "1\n" + "2\n" + "1\n" + "0\n";
+
+        assertTrue(AscendingOrDescending.sequence(new Scanner(input1)), "Test failed");
+        assertTrue(AscendingOrDescending.sequence(new Scanner(input2)), "Test failed");
+        assertTrue(!(AscendingOrDescending.sequence(new Scanner(input3))), "Test failed");
+        }
 }
