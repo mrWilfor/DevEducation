6b56f527d55f034b19fba6971b1852c61a88b7d1
Tarasov Ivan <mr.wilfor@gmail.com>
2020-05-05 20:53:17 +0300

    delete

--- src/main/java/HomeWork/hw_13_04_2020/HelloDevEduction.java
+++
@@ -1,9 +0,0 @@
-/*2. Find the example program “Hello, World!”, Output to the console “Hello, DevEducation!” */
-package HomeWork.hw_13_04_2020;
-
-public class HelloDevEduction {
-    public static void main(String[] args) {
-
-        System.out.println("Hello DevEducation!");
-    }
-}
