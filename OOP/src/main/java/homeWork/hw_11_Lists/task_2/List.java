9556c6e47802a96262f8ece83bb7751747dcf70c
Tarasov Ivan <mr.wilfor@gmail.com>
2020-06-12 09:22:44 +0300

    correct List interface

--- OOP/src/main/java/homeWork/hw_11_Lists/task_2/List.java
+++ OOP/src/main/java/homeWork/hw_11_Lists/task_2/List.java
@@ -1,17 +1,17 @@
 package homeWork.hw_11_Lists.task_2;
 
 public interface List {
-    Object get(int index);
+    Object get(int index) throws IndexOutOfBoundsException;
 
     boolean add(Object item);
 
     boolean addFirst(Object item);
 
-    boolean add(int index, Object item);
+    boolean add(int index, Object item) throws IndexOutOfBoundsException;
 
     boolean remove(Object item);
 
-    Object remove(int index);
+    Object remove(int index) throws IndexOutOfBoundsException;
 
     void clear();
 
